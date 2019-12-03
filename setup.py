a8e24a57e74f36913e38e1ad8b1714bbe7ecfc7f
GopikrishnanSasikumar <gopikrishnans1996@gmail.com>
2019-12-03 21:23:28 +0530

    Added setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    "pytest",
+    "requests",
+    "flask",
+    "gunicorn"
+]
+
+setup(
+    name="donation-page",
+    version="0.0.1",
+    zip_safe=False,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    install_requires=requirements)
