94e368bcb774af60bd616de8dc635eaf31f7b9ab
GopikrishnanSasikumar <gopikrishnans1996@gmail.com>
2020-04-15 14:55:11 +0530

    Taking keys as env variables

--- backend/app.py
+++ backend/app.py
@@ -4,8 +4,8 @@ import requests
 import razorpay
 import os
 
-#KEY = os.environ['KEY']
-#SECRET = os.environ['SECRET']
+KEY = os.environ['KEY']
+SECRET = os.environ['SECRET']
 client = razorpay.Client(auth=("<APP_ID>", "<APP_SECRET>"))
 app = Flask(__name__)
 @app.route('/health', methods=['GET'])
@@ -27,11 +27,11 @@ def donation_logic():
         session['amount'] = amount + '00'
         amount = int(session['amount'])
         order_id = create_order(amount)
-        return render_template('order.html', order_id=order_id, amount=session['amount']) 
+        return render_template('order.html', order_id=order_id, amount=session['amount'], key=key) 
     elif payment_type == "subscription":
         plans = client.plan.all()
         subscription_id = create_subscription(amount, plans)
-        return render_template('subscription.html', subscription_id=subscription_id, amount=session['amount'])
+        return render_template('subscription.html', subscription_id=subscription_id, amount=session['amount'], key=key)
 
 def create_order(amount):
     data = {
