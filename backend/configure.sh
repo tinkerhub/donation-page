6a751a0b95ed21fc2e3a73f6e307db9ceeec7c75
GopikrishnanSasikumar <gopikrishnans1996@gmail.com>
2020-04-19 16:07:16 +0530

    removed configure.sh from backend

--- backend/configure.sh
+++
@@ -1,6 +0,0 @@
-python3 setup.py install
-export RAZORPAY_KEY_TESTING="your key"
-export RAZORPAY_SECRET_TESTING="your secret key"
-export APP_SETTINGS=backend.coig.TestingConfig
-export FLASK_APP=backend/run.py
-flask run
