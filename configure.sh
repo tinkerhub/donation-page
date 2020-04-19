c9366dcf526e649a57b531810dd0bfaf64846313
GopikrishnanSasikumar <gopikrishnans1996@gmail.com>
2020-04-19 21:58:56 +0530

    Take key and secret as parameters

--- configure.sh
+++ configure.sh
@@ -1,6 +1,6 @@
 python3 setup.py install
-export RAZORPAY_KEY_TESTING=<KEY>
-export RAZORPAY_SECRET_TESTING=<SECRET>
+export RAZORPAY_KEY_TESTING=$1
+export RAZORPAY_SECRET_TESTING=$2
 export APP_SETTINGS=backend.config.TestingConfig
 export FLASK_APP=backend/run.py
 flask run
