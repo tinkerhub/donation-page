79c3828e0c65a6642501bbd0b7fea091d79bb906
GopikrishnanSasikumar <gopikrishnans1996@gmail.com>
2020-04-25 21:32:41 +0530

    Classed MailBoy

--- donationpage/mail_boy.py
+++ donationpage/mail_boy.py
@@ -1,14 +1,18 @@
 import requests
 from donationpage import app
-tinkerhub_subject = "Thank you for contributing to TinkerHub"
-tinkerhub_text= "Payment recieved.\n Thank you."
-tinkerhub_sender='Hakuna Matata<no-reply@community.tinkerhub.org>'
-def send_mail(reciever, sender=tinkerhub_sender, subject=tinkerhub_subject, text=tinkerhub_text):
-    return requests.post(
-        "https://api.mailgun.net/v3/community.tinkerhub.org/messages",
-        auth=("api", app.config['MAILGUN_API_KEY']),
-        data={"from": sender,
-              "to": [reciever],
-              "subject": subject,
-              "text": text})
+
+class MailBoy(object):
+    def __init__(self):
+        self.subject = "Thank you for contributing to TinkerHub"
+        self.text= "Hey\n Your valuable contribution to tinkerhub is recieved with love.\n Thank you."
+        self.sender='Hakuna Matata<no-reply@community.tinkerhub.org>'
+    
+    def send(self, reciever):
+        return requests.post(
+            "https://api.mailgun.net/v3/community.tinkerhub.org/messages",
+            auth=("api", app.config['MAILGUN_API_KEY']),
+            data={"from": self.sender,
+                "to": [reciever],
+                "subject": self.subject,
+                "text": self.text})
 
